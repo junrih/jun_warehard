b0216d97b08538be19b9b00ec232558b30ed92fb
unknown <jrizadajunrih08@gmail.com>
2024-03-01 08:58:24 -0800

    first repo commit

---
+++ templates/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Page Title</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<div class="header">
+  <h1>My Website</h1>
+  <p>A website created by me.</p>
+</div>
+
+<div class="navbar">
+  <a href="index.php">Home</a>
+  <a href="about.php">About Us</a>
+  <a href="cont.php">Contact Us</a>
+  <a href="login.php" class="right">Login</a>
+  
+</div>
